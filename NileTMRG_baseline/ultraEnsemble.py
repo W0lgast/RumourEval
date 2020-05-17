ddfe5dbe56a6fd2e35180cd649494677ceded3aa
Grant Stevens <grant-stevens@hotmail.co.uk>
2020-05-17 20:18:18 +0100

    gridsearch

--- NileTMRG_baseline/ultraEnsemble.py
+++ NileTMRG_baseline/ultraEnsemble.py
@@ -20,6 +20,8 @@ from sklearn.ensemble import VotingClassifier
 from nltk.classify.scikitlearn import SklearnClassifier
 from sklearn.calibration import CalibratedClassifierCV
 
+from sklearn.model_selection import GridSearchCV
+
 # -------------------------------------------------------------------------------------
 
 # class kippsSVC(LinearSVC):
@@ -31,17 +33,16 @@ from sklearn.calibration import CalibratedClassifierCV
 
 class TaskBEnsemble(object):
     def __init__(self, random_state=364):
-        self._models = [
-            MultinomialNB(),
-            DecisionTreeClassifier(random_state=random_state),
-            SVC(kernel="sigmoid", random_state=random_state, probability=True),
-            #LinearSVC(random_state=random_state),
-            CalibratedClassifierCV(
-                base_estimator=LinearSVC(random_state=random_state),
-                method='isotonic', cv=None  # your CV instance
+        self._models = {
+            # "MultinomialNB": MultinomialNB(),
+            # "DecisionTreeClassifier": DecisionTreeClassifier(random_state=random_state),
+            # "SVC": SVC(kernel="sigmoid", random_state=random_state, probability=True),
+            # LinearSVC(random_state=random_state),
+            "CalibratedClassifierCV": CalibratedClassifierCV(
+                base_estimator=LinearSVC(random_state=random_state), method="isotonic",
             ),
-            #SklearnClassifier(SVC(kernel='linear', probability=True, random_state=random_state)),
-            MLPClassifier(
+            # SklearnClassifier(SVC(kernel='linear', probability=True, random_state=random_state)),
+            "MLPClassifier": MLPClassifier(
                 hidden_layer_sizes=tuple([100] * 20),
                 max_iter=1000,
                 early_stopping=True,
@@ -59,11 +60,45 @@ class TaskBEnsemble(object):
             # RandomForestClassifier(
             #     n_estimators=10000, n_jobs=-1, random_state=random_state
             # ),
-        ]
+        }
+        self.__model_paramaters = {
+            "MultinomialNB": {
+                "alpha": [0.001, 0.2, 0.4, 0.6, 0.8, 1.0],
+                "fit_prior": (True, False),
+            },
+            "DecisionTreeClassifier": {"criterion": ("gini", "entropy")},
+            "SVC": {
+                "kernel": ("linear", "poly", "rbf", "sigmoid"),
+                "C": [0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000],
+                "degree": [2, 3, 4, 5],
+                "gamma": ("scale", "auto"),
+                "probability": [True],
+            },
+            "CalibratedClassifierCV": {
+                "base_estimator": [LinearSVC(random_state=random_state)],
+                "method": ["isotonic"],
+            },
+            "MLPClassifier": {
+                "hidden_layer_sizes": [(100,), (200,), (300,), (400,), (500,), (1000,)],
+                "activation": ("identity", "logistic", "tanh", "relu"),
+                "alpha": [0.0001, 0.001, 0.01, 0.1, 1, 10],
+                "learning_rate": ("constant", "invscaling", "adaptive"),
+                "learning_rate_init": [0.0001, 0.001, 0.01, 0.1, 1, 10, 100],
+            },
+        }
         self.__ensemble_model = []
 
     def fit(self, X, Y):
-        for model in self._models:
+        for i, model_name in enumerate(list(self._models.keys())):
+            model = self._models[model_name]
+            print(f"Running grid search on {model_name}...")
+            clf = GridSearchCV(
+                model, self.__model_paramaters[model_name], n_jobs=-1, verbose=2
+            )
+            clf.fit(X, Y)
+            print(f"{model} best params: {clf.best_params_}")
+            with open("gridsearch.txt", "a") as myfile:
+                myfile.write(f"{model} best params: {clf.best_params_}\n")
             print(f"Training model {type(model).__name__}")
             model.fit(X, Y)
 
